9d0aa8015031bc3a3a998b8aa99eaef1f2094e70
Kuba Franczuk <kuba@franczuk.pl>
2023-12-17 22:06:12 +0100

    fix: turned off reaqct-query-devtools

--- app/components/providers/ReactQueryProvider.tsx
+++ app/components/providers/ReactQueryProvider.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface Props {
   children: React.ReactNode;
@@ -12,7 +12,7 @@ const Provider = ({ children }: Props) => {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       {children}
     </QueryClientProvider>
   );
